8d0451fc10ac5e88ce954c00fb81f7e8a7a86d4d
v-dysand00 <andrew.dysart@vanamco.com>
2014-05-08 12:28:27 +0200

    update

---
+++ includes/_head.php
@@ -0,0 +1,19 @@
+        <!-- Latest compiled and minified CSS -->
+        <!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" > -->
+        <!-- Font Awesome -->
+
+        <!-- <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" > -->
+        <!-- Google Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
+
+        <!-- Custom Styles -->
+        <link rel="stylesheet" href="css/print.css"/>
+        <link rel="stylesheet" href="css/ie.css"/>
+        <link rel="stylesheet" href="css/screen.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+        <!--[if lt IE 9]>
+            <script type="text/javascript">
+                window.location = "ie.html ";
+            </script>
+        <![endif]-->
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="author" content="My name">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="//ajax.googleapis.com" rel="dns-prefetch">
+        <?php include"includes/_head.php"; ?>
+    </head>
+    <body>
+
+        <p>Hello world!</p>
+
+
+        <?php include"includes/_footer.php"; ?>
+
+        <?php include"includes/_scripts.php"; ?>
+
+    </body>
+</html>
